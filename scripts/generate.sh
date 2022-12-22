ebd7fd68b4dc7b236d1c43464ac90ecce1c8b9d3
Theo Mills <314020+twmills@users.noreply.github.com>
2022-12-22 14:23:19 -0600

    Install python first

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -14,6 +14,15 @@ if [ ! -e $RUBY_VERSION_NUM ] ; then
     ruby -v"
 fi
 
+if [ ! -e $PYTHON_VERSION_NUM ] ; then
+    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
+    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
+    rm Python-$PYTHON_VERSION_NUM.tgz && \
+    cd Python-$PYTHON_VERSION_NUM && \
+    ./configure && \
+    make install"
+fi
+
 if [ ! -e $NODE_VERSION_NUM ] ; then
     echo "RUN wget https://nodejs.org/dist/v$NODE_VERSION_NUM/node-v$NODE_VERSION_NUM.tar.gz && \
     tar -xzvf node-v$NODE_VERSION_NUM.tar.gz && \
@@ -24,15 +33,6 @@ if [ ! -e $NODE_VERSION_NUM ] ; then
     make install && \
     cd .. && \
     rm -r node-v$NODE_VERSION_NUM"
-fi
-
-if [ ! -e $PYTHON_VERSION_NUM ] ; then
-    echo "RUN wget https://www.python.org/ftp/python/$PYTHON_VERSION_NUM/Python-$PYTHON_VERSION_NUM.tgz && \
-    tar xzf Python-$PYTHON_VERSION_NUM.tgz && \
-    rm Python-$PYTHON_VERSION_NUM.tgz && \
-    cd Python-$PYTHON_VERSION_NUM && \
-    ./configure && \
-    make install"
 fi
 
 # if [ ! -e $PHP_VERSION_NUM ] ; then
